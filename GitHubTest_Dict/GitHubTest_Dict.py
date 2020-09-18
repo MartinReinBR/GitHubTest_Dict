bddbb76c1c910faa98841cef2d2ae59d6f02f6d6
MartinReinBR <70666306+MartinReinBR@users.noreply.github.com>
2020-09-18 13:54:55 +0200

    Add project files.

---
+++ GitHubTest_Dict/GitHubTest_Dict.py
@@ -0,0 +1,14 @@
+telRegister = {}
+for i in range(0,5):
+    inp = input("Mata in Namn och Telefonnummer spearerat med ett _: ")
+    parts = inp.split("_")
+    namn = parts[0]
+    tel = parts[1]
+    telRegister[namn] = tel
+
+while True:
+    namn = input("Mata in namn du vill söka på: ")
+    if namn in telRegister:
+        print(telRegister[namn])
+    else:
+        print("Namn inte hittat.")
